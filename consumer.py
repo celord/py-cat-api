d34f372c5b8539c9f6841fc30f894f2fd5245aab
Cesar Garcia <celord@gmail.com>
2022-05-01 22:03:41 -0600

    Step 13 50%

--- consumer.py
+++ consumer.py
@@ -2,8 +2,9 @@ from the_cat_api.cat_api import TheCatApi
 from the_cat_api.models import *
 catapi = TheCatApi()
 result = catapi.get_kitty()
-result = catapi.get_clowder_of_kitties(amt=6)
-print(result)
+print(result.url)
+# result = catapi.get_clowder_of_kitties(amt=6)
+# print(result)
 
 # from the_cat_api.rest_adapter import RestAdapter
 # from the_cat_api.models import *
--- the_cat_api/cat_api.py
+++ the_cat_api/cat_api.py
@@ -10,9 +10,7 @@ class TheCatApi:
             hostname, api_key, ver, ssl_verify, logger)
 
     def get_kitty(self) -> ImageShort:
-        result = self._rest_adapter.get(endpoint='/images/search')
-        kitty_img = ImageShort(**result.data[0])
-        return kitty_img
+        return self.get_clowder_of_kitties(amt=1)[0]
 
     def get_clowder_of_kitties(self, amt: int = 1) -> List[ImageShort]:
         params = {'limit': amt}
@@ -20,3 +18,31 @@ class TheCatApi:
             endpoint='/images/search', ep_params=params)
         kitty_img_list = [ImageShort(**datum) for datum in result.data]
         return kitty_img_list
+
+    def fetch_image_data(self, image: ImageShort):
+        image.data = self._rest_adapter.fetch_data(url=image.url)
+
+    def get_kitties_paged(self, max_amt: int = 100) -> Iterator[ImageShort]:
+        # Int variables
+        amt_yielded = 0
+        curr_page = last_page = 1
+        endpoint = '/images/search'
+        ep_params = {'limit': self._page_size, 'order': 'Desc'}
+
+        # Keep fetching pages of kitties until the last page
+        while curr_page <= last_page:
+            ep_params['page'] = curr_page
+            result = self._rest_adapter.get(
+                endpoint=endpoint, ep_params=ep_params)
+
+            # Increment curr_page by 1 and update the last_page based on header info returned
+            last_page = int(result.headers.get('pagination-count', 1))
+            curr_page = int(result.headers.get('pagination-page')) + 1
+
+            # Yield 1 kitty from the page; break/end loop if beyond max_amt
+            for datum in result.data:
+                yield ImageShort(**datum)
+                amt_yielded += 1
+                if amt_yielded >= max_amt:
+                    last_page = 0
+                    break
